2afe555e126cb380a72a0eff5ee2f89bc15d2ac5
TramvaichikA <tramvaichik@inbox.ru>
2024-12-20 19:40:43 +0300

    09.12.2024

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,45 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(printThreeWords());
+
+        System.out.println("1. Создать массив и подсчитать дубликаты");
+        Map<String, Integer> arrayMap=new HashMap<>();
+        String [] arr = {"Moscow", "Rostov", "Murmansk","Tula", "Habarovsk",
+                "Rostov", "Reutov", "Kaluga", "Voroneg", "Sankt-Peterburg",
+                "Moscow", "Stupino", "Korolev", "Moscow"};
+        for (int i = 0; i < arr.length; i++) {
+            if (arrayMap.containsKey(arr[i]))
+                arrayMap.put(arr[i], arrayMap.get(arr[i]) + 1);
+            else
+                arrayMap.put(arr[i], 1);
+        }
+        System.out.println("Массив из уникальных значений: \n" + arrayMap);
+
+        System.out.println();
+
+        System.out.println("2. Создать телефонный справочник. Найдем телефоны, принадлежащие следующим фамилиям: Smirnova и Ivanov");
+
+        org.example.PhoneGuide guide = new org.example.PhoneGuide();
+        guide.add("Sharova", "89990151226");
+        guide.add("Ivanov", "8926145327");
+        guide.add("Petrov", "8916150152");
+        guide.add("Suvorov", "8916450002");
+        guide.add("Romanov", "8903409993");
+        guide.add("Smirnov", "89102117711");
+        guide.add("Ivanov", "89263201920");
+        guide.add("Smirnova", "89038003115");
+        guide.add("Potapov", "89265402542");
+
+        System.out.println("Smirnova: " + guide.get("Smirnova"));
+        System.out.println("Ivanov: " + guide.get("Ivanov"));
 
     }
-      public static void printThreeWords()  {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
 
-      }
+
 }
